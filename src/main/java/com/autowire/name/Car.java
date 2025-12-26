fa94afff2947dbcd7f6a009adbf11497f6226e23
naseefu <naseefrahman90@gmail.com>
2025-12-26 23:35:21 +0530

    spring autowiring

---
+++ src/main/java/com/autowire/name/Car.java
@@ -0,0 +1,20 @@
+package com.autowire.name;
+
+
+public class Car {
+
+    private Specification specification;
+
+    /*public Car(Specification specification){
+        this.specification = specification;
+    }*/
+
+    public void setSpecification(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void getSpecificationOfCar(){
+        System.out.println(specification.toString());
+    }
+
+}
